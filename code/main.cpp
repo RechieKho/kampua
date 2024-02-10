6b3e55f8fa6edaac40b0e731643794972f3b9228
RechieKho <the.real.richie.kho@gmail.com>
2024-02-10 15:17:36 +0800

    Deduce metadata for when tokenizing.

--- code/main.cpp
+++ code/main.cpp
@@ -6,21 +6,21 @@ using namespace Parser;
 int main()
 {
     constexpr const TokenCutRule<char> cut_rules[] = {
-        TokenCutRule<char>(":=", 2, false),
-        TokenCutRule<char>(" ", 0, true)};
+        TokenCutRule<char>(":=", 2, TokenTypes::DEFAULT),
+        TokenCutRule<char>(" \n", 0, TokenTypes::OMITTED)};
     constexpr const TokenAmassRule<char> amass_rules[] = {
-        TokenAmassRule<char>('\'', '\'', true, false)};
-    const char *code = "var  something := 'some string.'";
+        TokenAmassRule<char>('\'', '\'', TokenTypes::DEFAULT, TokenTypes::DEFAULT)};
+    const char *code = "var something := 'some string.'\nvar another_thing := 'sometext'";
 
     try
     {
         const auto result = tokenize<char>(code, cut_rules, amass_rules);
         for (const auto &token : result)
         {
-            std::cout << '\'' << token << '\'' << '\n';
+            std::cout << '\'' << token.view << "\' [column: " << token.column << "; row: " << token.row << "]." << '\n';
         }
     }
-    catch (std::exception e)
+    catch (std::runtime_error e)
     {
         std::cout << "Error: " << e.what() << std::endl;
     }
--- code/parser/token.hpp
+++ code/parser/token.hpp
@@ -5,46 +5,68 @@
 #include <vector>
 #include <span>
 #include <stdexcept>
+#include <sstream>
 
 namespace Parser
 {
+    enum TokenTypes : std::size_t
+    {
+        OMITTED = 0,
+        DEFAULT = 1
+    };
 
-    template <typename T>
+    template <typename CharT>
     struct TokenCutRule
     {
-        std::basic_string_view<T> characters;
-        size_t max_count;
-        bool is_omitted;
+        std::basic_string_view<CharT> characters;
+        std::size_t max_count;
+        std::size_t type;
 
-        constexpr TokenCutRule(std::basic_string_view<T> p_characters, std::size_t p_max_count, bool p_is_omitted)
-            : characters(p_characters), max_count(p_max_count), is_omitted(p_is_omitted) {}
+        constexpr TokenCutRule(std::basic_string_view<CharT> p_characters, std::size_t p_max_count, std::size_t p_type)
+            : characters(p_characters), max_count(p_max_count), type(p_type) {}
         ~TokenCutRule() = default;
     };
 
-    template <typename T>
+    template <typename CharT>
     struct TokenAmassRule
     {
-        T starting_mark;
-        T ending_mark;
-        bool is_mark_omitted;
-        bool is_token_omitted;
+        CharT starting_mark;
+        CharT ending_mark;
+        std::size_t mark_type;
+        std::size_t content_type;
 
-        constexpr TokenAmassRule(T p_starting_mark, T p_ending_mark, bool p_is_mark_omitted, bool p_is_token_omitted)
-            : starting_mark(p_starting_mark), ending_mark(p_ending_mark), is_mark_omitted(p_is_mark_omitted), is_token_omitted(p_is_token_omitted) {}
+        constexpr TokenAmassRule(CharT p_starting_mark, CharT p_ending_mark, std::size_t p_mark_type, std::size_t p_content_type)
+            : starting_mark(p_starting_mark), ending_mark(p_ending_mark), mark_type(p_mark_type), content_type(p_content_type) {}
         ~TokenAmassRule() = default;
     };
 
-    template <typename T>
-    std::vector<std::basic_string_view<T>> tokenize(std::basic_string_view<T> p_code, std::span<const TokenCutRule<T>> p_cut_rules, std::span<const TokenAmassRule<T>> p_amass_rules)
+    template <typename CharT>
+    struct Token
     {
-        std::vector<std::basic_string_view<T>> tokens;
-        const TokenCutRule<T> *current_cut_rule_ptr = nullptr;
-        const TokenAmassRule<T> *current_amass_rule_ptr = nullptr;
-        std::basic_string_view<T> buffer(p_code.data(), 0);
+        std::basic_string_view<CharT> view;
+        std::size_t row;
+        std::size_t column;
+        std::size_t type;
+
+        constexpr Token(std::basic_string_view<CharT> p_view, std::size_t p_row, std::size_t p_column, std::size_t p_type)
+            : view(p_view), row(p_row), column(p_column), type(p_type) {}
+        ~Token() = default;
+    };
+
+    template <typename CharT>
+    std::vector<Token<CharT>> tokenize(std::basic_string_view<CharT> p_code, std::span<const TokenCutRule<CharT>> p_cut_rules, std::span<const TokenAmassRule<CharT>> p_amass_rules)
+    {
+        std::vector<Token<CharT>> tokens;
+        const TokenCutRule<CharT> *current_cut_rule_ptr = nullptr;
+        const TokenAmassRule<CharT> *current_amass_rule_ptr = nullptr;
+        std::basic_string_view<CharT> view(p_code.data(), 0);
+        std::size_t row = 1;
+        std::size_t column = 1;
 
         // Loop through code.
-        for (const T &character : p_code)
+        for (const CharT &character : p_code)
         {
+
             if (current_amass_rule_ptr != nullptr)
             {
                 // Amassing.
@@ -52,40 +74,45 @@ namespace Parser
                 // Check stop amassing.
                 if (character == current_amass_rule_ptr->ending_mark)
                 {
-                    // Append buffer.
-                    if (!current_amass_rule_ptr->is_token_omitted)
-                        if (buffer.size() != 0)
-                            tokens.push_back(buffer);
-                    if (!current_amass_rule_ptr->is_mark_omitted)
-                        tokens.push_back(std::basic_string_view<T>(&character, 1));
+                    // Stop amassing.
 
-                    // Setup buffer.
-                    buffer = std::basic_string_view<T>(&character + 1, 0);
+                    // Append view.
+                    if (current_amass_rule_ptr->content_type != TokenTypes::OMITTED && view.size() > 0)
+                        tokens.push_back(Token<CharT>(view, row, column - view.size(), current_amass_rule_ptr->content_type));
+                    if (current_amass_rule_ptr->mark_type != TokenTypes::OMITTED)
+                        tokens.push_back(Token<CharT>(std::basic_string_view<CharT>(&character, 1), row, column - view.size(), current_amass_rule_ptr->mark_type));
+
+                    // Setup view.
+                    view = std::basic_string_view<CharT>(&character + 1, 0);
 
                     // Stop amassing.
                     current_amass_rule_ptr = nullptr;
                 }
                 else
                     // Grow token.
-                    buffer = std::basic_string_view<T>(buffer.data(), buffer.size() + 1);
+                    view = std::basic_string_view<CharT>(view.data(), view.size() + 1);
             }
             else
             {
                 // Not amassing.
 
                 // Check start amassing.
-                for (const TokenAmassRule<T> &rule : p_amass_rules)
+                for (const TokenAmassRule<CharT> &rule : p_amass_rules)
                     if (character == rule.starting_mark)
                     {
-                        // Append buffer.
-                        if (current_cut_rule_ptr == nullptr || !current_cut_rule_ptr->is_omitted)
-                            if (buffer.size() != 0)
-                                tokens.push_back(buffer);
-                        if (!rule.is_mark_omitted)
-                            tokens.push_back(std::basic_string_view<T>(&character, 1));
+                        // Append view.
+                        if (view.size() > 0)
+                        {
+                            if (current_cut_rule_ptr == nullptr)
+                                tokens.push_back(Token<CharT>(view, row, column - view.size(), TokenTypes::DEFAULT));
+                            else if (current_cut_rule_ptr->type != TokenTypes::OMITTED)
+                                tokens.push_back(Token<CharT>(view, row, column - view.size(), current_cut_rule_ptr->type));
+                        }
+                        if (rule.mark_type != OMITTED)
+                            tokens.push_back(Token<CharT>(std::basic_string_view<CharT>(&character, 1), row, column - view.size(), rule.mark_type));
 
-                        // Setup buffer.
-                        buffer = std::basic_string_view<T>(&character + 1, 0);
+                        // Setup view.
+                        view = std::basic_string_view<CharT>(&character + 1, 0);
 
                         // Start amassing.
                         current_amass_rule_ptr = &rule;
@@ -93,25 +120,29 @@ namespace Parser
                     }
 
                 // Check cutting.
-                for (const TokenCutRule<T> &rule : p_cut_rules)
+                for (const TokenCutRule<CharT> &rule : p_cut_rules)
                     if (rule.characters.find(character) != rule.characters.npos)
                     {
-                        // Not default group.
+                        // Not default rule.
 
                         // Check in current rule.
                         if (current_cut_rule_ptr == nullptr || current_cut_rule_ptr != &rule)
                         {
                             // Not in current rule.
 
-                            // Append buffer.
-                            if (current_cut_rule_ptr == nullptr || !current_cut_rule_ptr->is_omitted)
-                                if (buffer.size() != 0)
-                                    tokens.push_back(buffer);
+                            // Append view.
+                            if (view.size() > 0)
+                            {
+                                if (current_cut_rule_ptr == nullptr)
+                                    tokens.push_back(Token<CharT>(view, row, column - view.size(), TokenTypes::DEFAULT));
+                                else if (current_cut_rule_ptr->type != TokenTypes::OMITTED)
+                                    tokens.push_back(Token<CharT>(view, row, column - view.size(), current_cut_rule_ptr->type));
+                            }
 
-                            // Setup buffer.
-                            buffer = std::basic_string_view<T>(&character, 1);
+                            // Setup view.
+                            view = std::basic_string_view<CharT>(&character, 1);
 
-                            // Start cutting.
+                            // Start cutting new rule.
                             current_cut_rule_ptr = &rule;
                         }
                         else
@@ -119,19 +150,19 @@ namespace Parser
                             // In current rule.
 
                             // Grow token.
-                            buffer = std::basic_string_view<T>(buffer.data(), buffer.size() + 1);
+                            view = std::basic_string_view<CharT>(view.data(), view.size() + 1);
 
                             // Check token reach maximum
-                            if (rule.max_count > 0 && buffer.size() >= rule.max_count)
+                            if (rule.max_count > 0 && view.size() >= rule.max_count)
                             {
                                 // Token reach maximum.
 
-                                // Append buffer.
-                                if (!rule.is_omitted && buffer.size() != 0)
-                                    tokens.push_back(buffer);
+                                // Append view.
+                                if (rule.type != TokenTypes::OMITTED && view.size() > 0)
+                                    tokens.push_back(Token<CharT>(view, row, column - view.size(), rule.type));
 
-                                // Setup buffer.
-                                buffer = std::basic_string_view<T>(&character + 1, 0);
+                                // Setup view.
+                                view = std::basic_string_view<CharT>(&character + 1, 0);
                             }
                         }
                         goto next_character;
@@ -144,14 +175,14 @@ namespace Parser
                 {
                     // Not in current rule.
 
-                    // Append buffer.
-                    if (!current_cut_rule_ptr->is_omitted && buffer.size() != 0)
-                        tokens.push_back(buffer);
+                    // Append view.
+                    if (current_cut_rule_ptr->type != TokenTypes::OMITTED && view.size() > 0)
+                        tokens.push_back(Token<CharT>(view, row, column - view.size(), current_cut_rule_ptr->type));
 
-                    // Setup buffer.
-                    buffer = std::basic_string_view<T>(&character, 1);
+                    // Setup view.
+                    view = std::basic_string_view<CharT>(&character, 1);
 
-                    // Start cutting.
+                    // Start cutting new rule.
                     current_cut_rule_ptr = nullptr;
                 }
                 else
@@ -159,24 +190,35 @@ namespace Parser
                     // In current rule.
 
                     // Grow token.
-                    buffer = std::basic_string_view<T>(buffer.data(), buffer.size() + 1);
+                    view = std::basic_string_view<CharT>(view.data(), view.size() + 1);
                 }
                 goto next_character;
             }
         next_character:;
+            column += 1;
+            if (character == CharT('\n'))
+            {
+                column = 0;
+                row += 1;
+            }
         }
 
         // Check unterminated amassing.
         if (current_amass_rule_ptr != nullptr)
         {
+            std::stringstream message;
+            message << "Unterminated character `" << current_amass_rule_ptr->ending_mark << "` at row " << row << ", column " << column << ".";
             // Raise error.
-            throw std::runtime_error(std::string("Unterminated character. [`") + current_amass_rule_ptr->ending_mark + "`]");
+            throw std::runtime_error(message.str().c_str());
         }
 
-        // Append buffer.
-        if (buffer.size() != 0)
+        // Append view.
+        if (view.size() > 0)
         {
-            tokens.push_back(buffer);
+            if (current_cut_rule_ptr == nullptr)
+                tokens.push_back(Token<CharT>(view, row, column - view.size(), TokenTypes::DEFAULT));
+            else if (current_cut_rule_ptr->type != TokenTypes::OMITTED)
+                tokens.push_back(Token<CharT>(view, row, column - view.size(), current_cut_rule_ptr->type));
         }
 
         return tokens;
