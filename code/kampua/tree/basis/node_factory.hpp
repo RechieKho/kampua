6284015946b0ceffdb4e433e16b4f20def866bfc
RechieKho <the.real.richie.kho@gmail.com>
2024-02-15 15:50:30 +0800

    Rename `Factory` to `NodeFactory`.

--- code/kampua/tree/basis/node_factory.hpp
+++ code/kampua/tree/basis/node_factory.hpp
@@ -1,35 +1,35 @@
-#ifndef KAMPUA_TREE_FACTORY_HPP
-#define KAMPUA_TREE_FACTORY_HPP
+#ifndef KAMPUA_TREE_BASIS_NODE_FACTORY_HPP
+#define KAMPUA_TREE_BASIS_NODE_FACTORY_HPP
 
 /**
  * @file
  *
- * @ingroup Kampua::Tree
+ * @ingroup Kampua::Tree::Basis
  *
  * @brief Base factory class to generate abstract syntax tree.
  */
 
-#include "../../concepts.hpp"
+#include "concepts.hpp"
 
-namespace Kampua::Tree
+namespace Kampua::Tree::Basis
 {
 
     //! @brief Base factory class to generate abstract syntax tree.
     //! @tparam T Character type
     template <typename T>
         requires Character<T>
-    class Factory
+    class NodeFactory
     {
     public:
-        virtual ~Factory() = 0;
+        virtual ~NodeFactory() = 0;
     };
 
     template <typename T>
         requires Character<T>
-    Factory<T>::~Factory()
+    NodeFactory<T>::~NodeFactory()
     {
     }
 
-} // namespace Kampua
+} // namespace Kampua::Tree::Basis
 
-#endif // KAMPUA_TREE_FACTORY_HPP
+#endif // KAMPUA_TREE_BASIS_NODE_FACTORY_HPP
